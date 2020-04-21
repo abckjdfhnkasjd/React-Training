a10bdc5e61ec872524421debf37f4d2c72980a22
abckjdfhnkasjd <gsprevolution.it@gmail.com>
2020-04-22 00:45:19 +0530

    Commiting code for question 6

--- react-assignment/question6/src/Admin.js
+++ react-assignment/question6/src/Admin.js
@@ -7,7 +7,8 @@ class Admin extends React.Component {
             <div className="row justify-content-center">
                 <div className="col-md-4 col-sm-12 col-xs-12"></div>
                 <div className="col-md-4 col-sm-12 col-xs-12">
-                    <h2>Admin page</h2>
+                    <h2 className="text-center">Login Successful</h2>
+                    <h3 className="text-center">Admin page</h3>
                 </div>
                 <div className="col-md-4 col-sm-12 col-xs-12"></div>
             </div>
