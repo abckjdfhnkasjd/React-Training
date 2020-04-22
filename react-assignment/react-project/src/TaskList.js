d2163f1bdad5d7b4fbc2016e9c72dce321739276
abckjdfhnkasjd <gsprevolution.it@gmail.com>
2020-04-22 14:45:03 +0530

    commting react-commit partially

--- react-assignment/react-project/src/TaskList.js
+++ react-assignment/react-project/src/TaskList.js
@@ -2,16 +2,15 @@ import React from 'react';
 import Task from './Task';
 
 class TaskList extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            taskList: [
-                {name: 'Task 1', status: 'In Progress', completed: false},
-                {name: 'Task 2', status: 'Completed', completed: true},
-                {name: 'Task 3', status: 'In Progress', completed: false}
-            ]
-        }
+    state = {
+        taskList: [
+            {name: 'Task 1', status: 'Active', completed: false},
+            {name: 'Task 2', status: 'Completed', completed: true},
+            {name: 'Task 3', status: 'Active', completed: false}
+        ],
+        tempTaskList: []
     }
+    
 
     deleteTaskHandler = (index) => {
         console.log('index:'+ index);
@@ -23,6 +22,21 @@ class TaskList extends React.Component{
         console.log('task:'+ this.state.taskList);
     }
     
+    renderTaskList = (param) => {
+        console.log(this.state.taskList);
+        let tempList = (this.state.tempTaskList.length === 0) ? this.state.taskList : this.state.tempTaskList;
+        let filteredTask = tempList.filter((task) => {
+            return (task.status === param);
+        });
+        if(param === 'All'){
+            filteredTask = tempList;
+        }
+        this.setState({
+            taskList: filteredTask,
+            tempTaskList: tempList
+        })
+    }
+
     render() {
         let taskArray = this.state.taskList.map((task, index) => {
             return <Task task={task} index= {index} key = {index} deleteTask = {this.deleteTaskHandler.bind(this)}/>
@@ -30,9 +44,22 @@ class TaskList extends React.Component{
         return (
             <div className="row">
                 <div className="col-xs-12">
-                    <ul className="list-group">
-                        {taskArray}
-                    </ul>
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <p> 
+                                <a onClick={(event) => { event.preventDefault();this.renderTaskList('All')}}>
+                                    All</a> | <a onClick={(event) => { event.preventDefault();this.renderTaskList('Completed')}}>
+                                        Completed</a> | <a onClick={(event) => { event.preventDefault();this.renderTaskList('Active')}}>
+                                            Active</a>
+                            </p>
+                        </div>
+                        <div class="panel-body">
+                            <ul className="list-group">
+                                {taskArray}
+                            </ul>
+                        </div>
+                        {/* <div class="panel-footer">Panel Footer</div> */}
+                    </div>
                 </div>
             </div>
         );
